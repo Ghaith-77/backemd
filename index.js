32d866003aad407304c934af24f683e2c1237b6f
yazan sawan <alisarter2@gmail.com>
2024-01-01 20:30:54 +0300

    adasd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let mongoose = require("mongoose");
 let app = express();
-let Article = require("./models/article")
+let Article = require("./models/article");
 mongoose
   .connect(
     "mongodb+srv://gaesx1x2:gaesx1x2@cluster0.t9sqeyn.mongodb.net/?retryWrites=true&w=majority"
@@ -30,16 +30,41 @@ app.get("/getnum", (req, res) => {
   });
 });
 
-app.post("/Article",(req,res)=>{
+app.post("/Article", (req, res) => {
   let newarticle = new Article()
   newarticle.title = req.body.title
   newarticle.body = req.body.body
   newarticle.save()
-  .then(()=>{
-    res.json(newarticle)
-
+    .then(() => {
+      res.json(newarticle)
+    })
+})
+app.get("/Article", async (req, res) => {
+  let newarticle = await Article.find()
+  res.json(newarticle)
+})
+app.get("/Article/:id", async (req, res) => {
+  let id  = req.params.id
+  let newarticle = await Article.findById(id)
+  res.json(newarticle)
+})
+app.delete("/Article/:id", async (req, res) => {
+  let id  = req.params.id
+  let newarticle = await Article.findByIdAndDelete(id)
+  res.json(newarticle)
+})
+app.get("/allArticle", async (req, res) => {
+  let newarticle = await Article.find()
+  res.render("article.ejs",{
+    articles : newarticle
   })
 })
+// app.put("/Article/:id", async (req, res) => {
+//   let id  = req.params.id
+//   let newarticle = await Article.findByIdAndUpdate(id)
+//   res.json(newarticle)
+// })
+
 app.get("/saymyname", (req, res) => {
   let age = req.query.age;
   let name = req.body.name;
